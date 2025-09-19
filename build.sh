d328c95b2bb87a0503c35405d9e613fb885ec622
WerX <arturturtur1234@gmail.com>
2025-09-19 19:39:05 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+set -e
+
 # создаём папки
 mkdir -p bin iso/boot
 
-# компиляция ядра
+# --- компиляция ядра ---
 gcc -m32 -ffreestanding -fno-pie -O2 -c kernel/kernel.c -o bin/kernel.o
-
-# линковка
-ld -m elf_i386 -T boot/linker.ld -o bin/kernel.bin bin/kernel.o -e _start
-
-# копирование
+ld  -m elf_i386 -T boot/linker.ld -o bin/kernel.bin bin/kernel.o -e _start
 cp bin/kernel.bin iso/boot/
+
+# --- компиляция recovery ---
+gcc -m32 -ffreestanding -fno-pie -O2 -c kernel/recovery.c -o bin/recovery.o
+ld  -m elf_i386 -T boot/linker.ld -o bin/recovery.bin bin/recovery.o -e _start
+cp bin/recovery.bin iso/boot/
+
+# --- копирование SystemRoot ---
 cp -r systemroot iso/SystemRoot
 
-# сборка ISO
-grub-mkrescue -d /usr/lib/grub/i386-pc -o wexos.iso iso
+# --- сборка ISO ---
+grub-mkrescue -o bin/wexos.iso iso
 
-echo "Сборка завершена!"
+echo "Сборка завершена! Файл: bin/wexos.iso"
