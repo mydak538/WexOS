3c1cce43780515f975804222e45a5bcd00c43690
WerX <arturturtur1234@gmail.com>
2025-09-20 09:55:51 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -e
 
 # создаём папки
 mkdir -p bin iso/boot
 
-# --- компиляция ядра ---
+# --- компиляция kernel ---
 gcc -m32 -ffreestanding -fno-pie -O2 -c kernel/kernel.c -o bin/kernel.o
 ld  -m elf_i386 -T boot/linker.ld -o bin/kernel.bin bin/kernel.o -e _start
 cp bin/kernel.bin iso/boot/
@@ -14,6 +13,11 @@ cp bin/kernel.bin iso/boot/
 gcc -m32 -ffreestanding -fno-pie -O2 -c kernel/recovery.c -o bin/recovery.o
 ld  -m elf_i386 -T boot/linker.ld -o bin/recovery.bin bin/recovery.o -e _start
 cp bin/recovery.bin iso/boot/
+
+# --- компиляция install ---
+gcc -m32 -ffreestanding -fno-pie -O2 -c kernel/install.c -o bin/install.o
+ld  -m elf_i386 -T boot/linker.ld -o bin/install.bin bin/install.o -e _start
+cp bin/install.bin iso/boot/
 
 # --- копирование SystemRoot ---
 cp -r systemroot iso/SystemRoot
